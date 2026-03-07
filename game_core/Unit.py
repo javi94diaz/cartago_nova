42a20fd64e1bf67d6da429c72b2047750e549cf4
Javier Díaz <javidiaz94@gmail.com>
2026-03-07 17:24:10 +0100

    Include global counter for units of each type

--- game_core/Unit.py
+++ game_core/Unit.py
@@ -1,5 +1,8 @@
 import uuid
 class Unit:
+    
+    _type_counters = {}
+    
     def __init__(self, unit_type, owner, zone):
         
         self.id = uuid.uuid4()
@@ -7,6 +10,14 @@ class Unit:
         self.type = unit_type
         self.owner = owner
         self.zone = zone
+
+        name = unit_type.name
+        if name not in Unit._type_counters:
+            Unit._type_counters[name] = 0
+
+        Unit._type_counters[name] += 1
+        self.number = Unit._type_counters[name]
+
         self.health = unit_type.max_health
         self.current_health = unit_type.max_health
         # self.has_moved = False
@@ -14,6 +25,6 @@ class Unit:
         self.alive = True
 
     def __repr__(self):
-        return f"<Unit {self.type} ({self.owner.faction})>"
+        return f"<Unit {self.type.name}#{self.number} {self.owner.faction} @{self.zone.name}>"
 
         
