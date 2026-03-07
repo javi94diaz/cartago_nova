49bbf81d793bd214a578ad4e8e8ab2356b269eaf
Javier Díaz <javidiaz94@gmail.com>
2026-03-07 17:24:38 +0100

    Add Zone capacity for max number of units

--- game_core/Zone.py
+++ game_core/Zone.py
@@ -1,10 +1,11 @@
 
 class Zone:
-    def __init__(self, name, x , y):
+    def __init__(self, name, x , y, capacity):
         print(f"[Zone] Create Zone {name}")
         self.name = name
         self.x = x
         self.y = y
+        self.capacity = capacity
         self.units = []
         self.adjacent = set()   # Neighbor zones
 
