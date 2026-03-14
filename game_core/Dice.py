43c292cd876bfbe844887478a55afb6bb3cd2f6f
Javier Díaz <javidiaz94@gmail.com>
2026-03-14 17:58:42 +0100

    Add multi-side dice support

--- game_core/Dice.py
+++ game_core/Dice.py
@@ -3,5 +3,9 @@ import random
 class Dice:
 
     @staticmethod
+    def roll(sides):
+        return random.randint(1, sides)
+
+    @staticmethod
     def d6():
-        return random.randint(1,6)
+        return Dice.roll(6)
