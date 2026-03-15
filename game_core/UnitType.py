4a5e14fe86461d9b2afb119eede9bd38b8279011
Javier Díaz <javidiaz94@gmail.com>
2026-03-15 17:31:46 +0100

    Minor fix

--- game_core/UnitType.py
+++ game_core/UnitType.py
@@ -2,7 +2,7 @@
 class UnitType:
     def __init__(self, name, max_health, combat_dice, shots, hit_number):
         self.name = name
-        self.max_health = max_health        
+        self.max_health = max_health
         self.combat_dice = combat_dice
         self.shots = shots
         self.hit_number = hit_number
