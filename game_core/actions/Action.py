a97fc28834077d590ff28048e5df89d3325bd252
Javier Díaz <javidiaz94@gmail.com>
2026-04-01 01:44:15 +0200

    Add unique id for each Action

--- game_core/actions/Action.py
+++ game_core/actions/Action.py
@@ -1,6 +1,18 @@
 from abc import ABC, abstractmethod
 
 class Action(ABC):
+    _next_id = 1
+
+    def __init__(self):
+        self.id = Action._next_id
+        Action._next_id += 1
+
+    def describe(self):
+        return ""
+
+    def __repr__(self):
+        description = self.describe()
+        return f"<{self.__class__.__name__}#{self.id} {description}>"
 
     @abstractmethod
     def validate(self):
